0838719771758d0a2ff6000567704f58a1673845
Baiaman Urmatbek <urmatbay@gmail.com>
2015-10-12 11:18:52 -0700

    csv writer is integrated

--- ReGeocode.py
+++ ReGeocode.py
@@ -7,6 +7,8 @@ from random import randint
 import csv
 
 
+# lat_lng = open_with_csv('lat_lng.csv')
+
 # Create and write in csv file.
 with open( "my_address_components.csv", "w") as f:
 	output = csv.writer(f)
@@ -15,16 +17,21 @@ with open( "my_address_components.csv", "w") as f:
 # Google Reverse Geocoding.
 	for ll in lat_lng:
 		# sleeps
-		time.sleep(randint(1,2)) 
+		time.sleep(.01)
 
-		def get_address():		
-			raw_address = requests.get('http://maps.googleapis.com/maps/api/geocode/json?latlng=' + ll)
-			full_address = raw_address.json()
-			my_full_address = (full_address['results'][0]['formatted_address'])
-			output.writerow([i, my_full_address])
+		def get_address():
+			try:
+				raw_address = requests.get('http://maps.googleapis.com/maps/api/geocode/json?latlng=' + ll)
+				full_address = raw_address.json()
+				my_full_address = (full_address['results'][0]['formatted_address'])
+				output.writerow([i, my_full_address])
+				print("Success!! {}".format(i))
+			except:
+				print("N/A {}".format(i))
+				output.writerow([i, "N/A"])
+				pass
 
 		get_address()		
-		print("Success!! {}".format(i))
 		i += 1
 
 
--- lat_lng.py
+++ lat_lng.py
@@ -1,3 +1,5 @@
+import csv
+
 lat_lng = [
 "49.153485,-123.133912",
 "49.265854,-123.168001",
@@ -9,3 +11,16 @@ lat_lng = [
 "49.079342,-122.65824",
 "49.169092,-121.937449"
 ]
+
+
+def open_with_csv(filename):
+	data = []
+	with open(filename, encoding='utf-8') as tsvin:
+		reader = csv.reader(tsvin)
+		for line in  reader:
+			data.append(line)
+	return data
+
+if __name__ == '__main__':
+	lat_lng = open_with_csv('lat_lng.csv')
+	print(lat_lng)
